b0c4c9521ebd3c09a64671941a8fd837531b4781
Leo Liao <Leo_HC_Liao@dell.com>
2020-09-03 00:41:09 +0800

    initial module files

---
+++ MMM-TwBusEta.js
@@ -0,0 +1,257 @@
+/*
+
+sample module structure
+
+
+ */
+
+const header = ["symbol", "route", "close", "change", "changeP", "volume"]
+const headerTitle = ["Symbol", "Cur.Price", "Prev.Close", "CHG", "CHG%", "Volume"]
+
+Module.register("MMM-TwBusEta", {
+    // define variables used by module, but not in config data
+    //some_variable:  true,
+    //some_other_variable: "a string",
+
+    // holder for config info from module_name.js
+    //config:null,
+
+    // anything here in defaults will be added to the config data
+    // and replaced if the same thing is provided in config
+    defaults: {
+      apiKey : "254",
+      timeFormat: "DD-MM HH:mm",
+      symbols : ["Taipei"],
+      tickerDuration: 60,
+      chartDays: 90,
+    },
+
+/*    init: function(){
+        Log.log(this.name + " is in init!");
+    },*/
+
+    start: function(){
+        Log.log(this.name + " is starting!");
+        this.sendSocketNotification("INIT", this.config)
+        this.stocks = {}
+        this.isStarted = false
+    },
+
+/*    loaded: function(callback) {
+        Log.log(this.name + " is loaded!");
+        callback();
+    },*/
+
+    // return list of other functional scripts to use, if any (like require in node_helper)
+    getScripts: function() {
+    return  [
+            // sample of list of files to specify here, if no files,do not use this routine, or return empty list
+
+            //'script.js', // will try to load it from the vendor folder, otherwise it will load is from the module folder.
+            //'moment.js', // this file is available in the vendor folder, so it doesn't need to be available in the module folder.
+            //this.file('anotherfile.js'), // this file will be loaded straight from the module folder.
+            //'https://code.jquery.com/jquery-2.2.3.min.js',  // this file will be loaded from the jquery servers.
+        ]
+    }, 
+
+    // return list of stylesheet files to use if any
+    getStyles: function() {
+        return ["MMM-AVStock.css"]
+    },
+/*
+    getStyles: function() {
+        return  [
+            // sample of list of files to specify here, if no files, do not use this routine, , or return empty list
+
+            //'script.css', // will try to load it from the vendor folder, otherwise it will load is from the module folder.
+            //'font-awesome.css', // this file is available in the vendor folder, so it doesn't need to be avialable in the module folder.
+            //this.file('anotherfile.css'), // this file will be loaded straight from the module folder.
+            //'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css',  // this file will be loaded from the bootstrapcdn servers.
+        ]
+    },
+*/
+    // return list of translation files to use, if any
+    /*getTranslations: function() {
+        return {
+            // sample of list of files to specify here, if no files, do not use this routine, , or return empty list
+
+            // en: "translations/en.json",  (folders and filenames in your module folder)
+            // de: "translations/de.json"
+        }
+    }, */ 
+
+
+
+    // only called if the module header was configured in module config in config.js
+    getHeader: function() {
+        return this.data.header + " Foo Bar";
+    },
+
+    // messages received from other modules and the system (NOT from your node helper)
+    // payload is a notification dependent data structure
+    notificationReceived: function(noti, payload) {
+        if (noti == "DOM_OBJECTS_CREATED") {
+            this.sendSocketNotification("START")
+            this.prepare()
+        }
+    },
+/*
+    notificationReceived: function(notification, payload, sender) {
+        // once everybody is loaded up
+        if(notification==="ALL_MODULES_STARTED"){
+            // send our config to our node_helper
+            this.sendSocketNotification("CONFIG",this.config)
+        }
+        if (sender) {
+            Log.log(this.name + " received a module notification: " + notification + " from sender: " + sender.name);
+        } else {
+            Log.log(this.name + " received a system notification: " + notification);
+        }
+    },
+*/
+    // messages received from from your node helper (NOT other modules or the system)
+    // payload is a notification dependent data structure, up to you to design between module and node_helper
+    socketNotificationReceived: function(noti, payload) {
+        if (noti == "UPDATE") {
+            if (payload.hasOwnProperty('symbol')) {
+                this.stocks[payload.symbol] = payload
+                this.update(payload)
+            }
+        }
+        //if (noti == "UPDATE_SERIES") {
+        //    this.updateSeries(payload)
+        //}
+    },
+/*
+    socketNotificationReceived: function(notification, payload) {
+        Log.log(this.name + " received a socket notification: " + notification + " - Payload: " + payload);
+        if(notification === "message_from_helper"){
+            this.config.message = payload;
+            // tell mirror runtime that our data has changed,
+            // we will be called back at GetDom() to provide the updated content
+            this.updateDom(1000)
+        }
+
+    },
+*/
+    update: function(stock) {
+        this.drawTable(stock)
+    },
+
+    drawTable: function(stock) {
+        var hash = stock.hash
+        var tr = document.getElementById("STOCK_" + hash)
+        var ud = ""
+        for (j = 1 ; j <= 5 ; j++) {
+            var tdId = header[j] + "_" + hash
+            var td = document.getElementById(tdId)
+            td.innerHTML = stock[header[j]]
+            td.className = header[j]
+            if (header[j] == "change") {
+                if (stock[header[j]] > 0) {
+                    ud = "up"
+                } else if (stock[header[j]] < 0) {
+                    ud = " down"
+                }
+            }
+        }
+        tr.className = "animated stock " + ud
+        var tl = document.getElementById("AVSTOCK_TAGLINE")
+        tl.innerHTML = "Last updated: " + stock.requestTime
+        setTimeout(()=>{
+            tr.className = "stock " + ud
+        }, 1500);
+    },
+
+    // system notification your module is being hidden
+    // typically you would stop doing UI updates (getDom/updateDom) if the module is hidden
+    suspend: function(){
+
+    },
+
+    // system notification your module is being unhidden/shown
+    // typically you would resume doing UI updates (getDom/updateDom) if the module is shown
+    resume: function(){
+
+    },
+
+    // this is the major worker of the module, it provides the displayable content for this module
+    getDom: function() {
+        var wrapper = document.createElement("div")
+        wrapper.id = "AVSTOCK"
+        return wrapper
+    },
+
+/*
+    getDom: function() {
+        var wrapper = document.createElement("div");
+
+        // if user supplied message text in its module config, use it
+        if(this.config.hasOwnProperty("message")){
+            // using text from module config block in config.js
+            wrapper.innerHTML = this.config.message;
+        }
+        else{
+        // use hard coded text
+            wrapper.innerHTML = "Hello world!";
+        }
+
+        // pass the created content back to MM to add to DOM.
+        return wrapper;
+    },
+*/
+
+    prepare: function() {
+        this.prepareTable()
+    },
+/*
+    getStockName: function(symbol) {
+        var stockAlias = symbol
+        var i = this.config.symbols.indexOf(symbol)
+        if (this.config.symbols.length == this.config.alias.length) {
+            stockAlias = (this.config.alias[i]) ? this.config.alias[i] : stockAlias
+        }
+        return stockAlias
+    },
+*/
+    prepareTable: function() {
+        var wrapper = document.getElementById("AVSTOCK")
+        wrapper.innerHTML = ""
+
+        var tbl = document.createElement("table")
+        tbl.id = "AVSTOCK_TABLE"
+        var thead = document.createElement("thead")
+        var tr = document.createElement("tr")
+        for (i in header) {
+            var td = document.createElement("td")
+            td.innerHTML = headerTitle[i]
+            td.className = header[i]
+            tr.appendChild(td)
+        }
+        thead.appendChild(tr)
+        tbl.appendChild(thead)
+
+        for (i in this.config.symbols) {
+            var stock = this.config.symbols[i]
+            var hashId = stock.hashCode()
+            var tr = document.createElement("tr")
+            tr.className = "stock"
+            tr.id = "STOCK_" + hashId
+            for (j in header) {
+                var td = document.createElement("td")
+                var stockAlias = stock
+                td.innerHTML = (j != 0) ? "---" : stockAlias
+                td.className = header[j]
+                td.id = header[j] + "_" + hashId
+                tr.appendChild(td)
+            }
+            tbl.appendChild(tr)
+        }
+        wrapper.appendChild(tbl)
+        var tl = document.createElement("div")
+        tl.className = "tagline"
+        tl.id = "AVSTOCK_TAGLINE"
+        tl.innerHTML = "Last updated : "
+        wrapper.appendChild(tl)
+    },
+})
---
+++ node_helper.js
@@ -0,0 +1,215 @@
+var NodeHelper = require("node_helper");
+
+// add require of other javascripot components here
+//const axios = require('axios');
+//const jsSHA = require('jssha');
+//const fs = require('fs');
+//const iconv = require('iconv-lite');
+//const utf8 = require('utf8');
+/* //do i need this ?
+const getAuthorizationHeader = function() {
+    var AppID = '303107a2014d4fb1b6eb8a01306d3ea4';
+    var AppKey = '38SirQ54STAARFAf951GZPWeGv8';
+
+    var GMTString = new Date().toGMTString();
+    var ShaObj = new jsSHA('SHA-1', 'TEXT');
+    ShaObj.setHMACKey(AppKey, 'TEXT');
+    ShaObj.update('x-date: ' + GMTString);
+    var HMAC = ShaObj.getHMAC('B64');
+    var Authorization = 'hmac username=\"' + AppID + '\", algorithm=\"hmac-sha1\", headers=\"x-date\", signature=\"' + HMAC + '\"';
+
+    return { 'Authorization': Authorization, 'X-Date': GMTString};
+}
+*/
+
+module.exports = NodeHelper.create({
+
+    init(){
+        console.log("init module helper SampleModule");
+        this.config = null
+        this.pooler = []
+        this.doneFirstPooling = false
+    },
+
+    start() {
+        console.log('Starting module helper:' +this.name);
+        this.config = null
+        this.pooler = []
+        this.doneFirstPooling = false
+    },
+
+    stop(){
+        console.log('Stopping module helper: ' +this.name);
+    },
+
+    socketNotificationReceived: function(noti, payload) {
+        if (noti == "INIT") {
+            this.config = payload
+            console.log("[TWBUSETA] Initialized.")
+        }
+        if (noti == "START") {
+            if (this.pooler.length == 0) {
+                this.prepareScan()
+            }
+            this.startPooling()
+        }
+    },
+    startPooling: function() {
+        // Since December 2018, Alphavantage changed API quota limit.(500 per day)
+        // So, fixed interval is used. for the first cycle, 15sec is used.
+        // After first cycle, 3min is used for interval to match 500 quota limits.
+        // So, one cycle would be 3min * symbol length;
+        var interval = 0
+        if (this.config.premiumAccount) {
+          interval = this.config.poolInterval
+        } else {
+          interval = (this.doneFirstPooling) ? 180000 : 15000
+        }
+
+        if (this.pooler.length > 0) {
+          var symbol = this.pooler.shift()
+          this.callAPI(this.config, symbol, (noti, payload)=>{
+            this.sendSocketNotification(noti, payload)
+          })
+        } else {
+          this.doneFirstPooling = true
+          this.prepareScan()
+        }
+
+        var timer = setTimeout(()=>{
+          this.startPooling()
+        }, interval)
+    },
+    callAPI: function(cfg, symbol, callback) {
+        var url = ""
+        //https://ptx.transportdata.tw/MOTC/v2/Bus/EstimatedTimeOfArrival/City/Taipei/254?$top=30&$format=JSON
+        url = "https://ptx.transportdata.tw/MOTC/v2/Bus/EstimatedTimeOfArrival/City/"
+        // symbol = city
+        // cfg.apiKey = route
+        url += symbol + "/" + cfg.apiKey + "?$top=1&$format=JSON"
+        //url += symbol + "?$top=30&$format=JSON"
+
+        request(url, (error, response, body) => {
+            //console.log("[AVSTOCK] API is called - ", symbol)
+            var data = null
+            if (error) {
+                console.log("[TWBUSETA] API Error: ", error)
+                return
+            }
+            data = JSON.parse(body)
+            if (data.hasOwnProperty("Note")) {
+                console.log("[TWBUSETA] Error: API Call limit exceeded.")
+            }
+            if (data.hasOwnProperty("Error Message")) {
+                console.log("[TWBUSETA] Error:", data["Error Message"])
+            }
+            //if (!data["Global Quote"].hasOwnProperty("01. symbol")) {
+            //  console.log("[TWBUSETA] Data Error: There is no available data for", symbol)
+            //}
+            //console.log("[AVSTOCK] Response is parsed - ", symbol)
+            var series = data
+            var keys = Object.keys(series)
+            //var dayLimit = (cfg.chartDays > 90) ? 90 : cfg.chartDays
+            //var keys = keys.sort().reverse().slice(0, dayLimit)
+            var ts = []
+            for (k in keys) {
+                //var index = keys[k]
+                var item = {
+                    "symbol": symbol,
+                    "date": index,
+                    "stopid": series[k]["StopUID"],
+                    "stopname": series[k]["StopName"][0],
+                    "routename": series[k]["RouteName"][0],
+                    "direction": series[k]["Direction"],
+                    "estimate": series[k]["EstimateTime"],
+                    "stopstatus": series[k]["StopStatus"],
+                }
+                //item.candle = ((item.close - item.open) >= 0) ? "up" : "down"
+                ts.push(item)
+            }
+            callback('UPDATE_SERIES', ts)
+/*
+   {
+      "StopUID":"TPE17678",
+      "StopID":"17678",
+      "StopName":{
+         "Zh_tw":"師大",
+         "En":"National Taiwan Normal U."
+      },
+      "RouteUID":"TPE10783",
+      "RouteID":"10783",
+      "RouteName":{
+         "Zh_tw":"254",
+         "En":"254"
+      },
+      "Direction":1,
+      "EstimateTime":85,
+      "StopStatus":0,
+      "MessageType":0,
+      "SrcUpdateTime":"2020-09-02T23:52:20+08:00",
+      "UpdateTime":"2020-09-02T23:52:26+08:00"
+   },
+   {
+      "StopUID":"TPE17578",
+      "StopID":"17578",
+      "StopName":{
+         "Zh_tw":"景平路景德街口",
+         "En":"Jingxin New Village"
+      },
+      "RouteUID":"TPE10783",
+      "RouteID":"10783",
+      "RouteName":{
+         "Zh_tw":"254",
+         "En":"254"
+      },
+      "Direction":0,
+      "StopStatus":3,
+      "MessageType":0,
+      "SrcUpdateTime":"2020-09-02T23:52:20+08:00",
+      "UpdateTime":"2020-09-02T23:52:26+08:00"
+   },
+{
+BusN1EstimateTime {
+    StopUID (string, optional): 站牌唯一識別代碼，規則為 {業管機關簡碼} + {StopID}，其中 {業管機關簡碼} 可於Authority API中的AuthorityCode欄位查詢 ,
+    StopID (string, optional): 地區既用中之站牌代碼(為原資料內碼) ,
+    StopName (NameType, optional): 站牌名 ,
+    RouteUID (string, optional): 路線唯一識別代碼，規則為 {業管機關代碼} + {RouteID}，其中 {業管機關代碼} 可於Authority API中的AuthorityCode欄位查詢 ,
+    RouteID (string, optional): 地區既用中之路線代碼(為原資料內碼) ,
+    RouteName (NameType, optional): 路線名稱 ,
+    Direction (integer): 去返程(該方向指的是此車牌車輛目前所在路線的去返程方向，非指站站牌所在路線的去返程方向，使用時請加值業者多加注意) : [0:'去程',1:'返程',2:'迴圈',255:'未知'] ,
+    EstimateTime (integer, optional): 到站時間預估(秒) [當StopStatus値為2~4或PlateNumb値為-1時，EstimateTime値為null; 當StopStatus値為1時， EstimateTime値多數為null，僅部分路線因有固定發車時間，故EstimateTime有値; 當StopStatus値為0時，EstimateTime有値。] ,
+    StopStatus (integer, optional): 車輛狀態備註 : [0:'正常',1:'尚未發車',2:'交管不停靠',3:'末班車已過',4:'今日未營運'] ,
+    MessageType (integer, optional): 資料型態種類 : [0:'未知',1:'定期',2:'非定期'] ,
+
+    SrcUpdateTime (DateTime, optional): 來源端平台資料更新時間(ISO8601格式:yyyy-MM-ddTHH:mm:sszzz)[公總使用動態即時推播故沒有提供此欄位, 而非公總系統因提供整包資料更新, 故有提供此欄] ,
+    UpdateTime (DateTime): 本平台資料更新時間(ISO8601格式:yyyy-MM-ddTHH:mm:sszzz)
+}
+*/
+
+        })
+    },
+
+    prepareScan: function() {
+        for (s in this.config.symbols) {
+            var symbol = this.config.symbols[s]
+            this.pooler.push(symbol)
+        }
+    },
+/*
+    // handle messages from our module// each notification indicates a different messages
+    // payload is a data structure that is different per message.. up to you to design this
+    socketNotificationReceived(notification, payload) {
+        console.log(this.name + " received a socket notification: " + notification + " - Payload: " + payload);
+        // if config message from module
+        if (notification === "CONFIG") {
+            // save payload config info
+            this.config=payload
+            // wait 15 seconds, send a message back to module
+            setTimeout(()=> { this.sendSocketNotification("message_from_helper"," this is a test_message")}, 15000)
+        }
+        else if(notification === "????2") {
+        }
+
+    },
+*/
+});
